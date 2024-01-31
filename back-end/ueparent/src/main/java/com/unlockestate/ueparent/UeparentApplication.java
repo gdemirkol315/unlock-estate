babd17a16afcb55406a8156c63845e34b1451438
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-01-31 12:04:24 +0100

    spring app added from spring intlzr

---
+++ back-end/ueparent/src/main/java/com/unlockestate/ueparent/UeparentApplication.java
@@ -0,0 +1,13 @@
+package com.unlockestate.ueparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeparentApplication.class, args);
+	}
+
+}
---
+++ back-end/ueparent/src/test/java/com/unlockestate/ueparent/UeparentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlockestate.ueparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeparentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
