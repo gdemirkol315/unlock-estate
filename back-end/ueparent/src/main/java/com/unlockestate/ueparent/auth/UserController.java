b74155eccc9adfedf25a266e8475f8f636a98470
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-02-08 16:42:37 +0100

    logging added, path fix

--- back-end/ueparent/src/main/java/com/unlockestate/ueparent/auth/UserController.java
+++ back-end/ueparent/src/main/java/com/unlockestate/ueparent/auth/UserController.java
@@ -1,10 +1,8 @@
 package com.unlockestate.ueparent.auth;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins= "http://localhost:4200")
 @RestController
 public class UserController {
 
--- front-end/src/app/services/auth/auth.service.ts
+++ front-end/src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {DataService} from "../data/data.service";
 import {User} from "../../models/user.model";
+import {HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,10 @@ import {User} from "../../models/user.model";
 export class AuthService extends DataService {
 
   createUser(user: User) {
-    return this.http.post(this.hostname + "createUser", User);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+      // Add other headers as needed
+    });
+    return this.http.post(this.hostname + "createUser", user, {headers});
   }
 }
