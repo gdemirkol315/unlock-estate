849be7c8418f9db97202bb884efba8717b6de7c0
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-02-28 11:41:00 +0100

    adjustments for prod

--- front-end/src/environments/environment.dev.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  title: 'Local Environment Heading',
-  apiURL: 'http://localhost:8080/ueapp/'
-};
--- front-end/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  title: 'Web Environment Heading',
-  apiURL: ''
-};
