419a84068854741325c20757a042a9b6d8060a88
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-04-01 16:32:29 +0200

    Api exception handled

--- back-end/ueparent/src/main/java/com/unlockestate/ueparent/notification/service/WhatsAppService.java
+++ back-end/ueparent/src/main/java/com/unlockestate/ueparent/notification/service/WhatsAppService.java
@@ -1,6 +1,7 @@
 package com.unlockestate.ueparent.notification.service;
 
 import com.twilio.Twilio;
+import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.account.Message;
 import com.unlockestate.ueparent.notification.config.TwilioConfig;
 import jakarta.annotation.PostConstruct;
@@ -27,12 +28,19 @@ public class WhatsAppService {
 
 
     public void sendMessage(String messageContent, String toNumber) {
+        try {
+            Message message = sendWhatsAppMessage(messageContent, toNumber);
+            logger.info("Whatsapp Message Status: {}", message.getStatus());
+        } catch (ApiException e) {
+            logger.error("Error sending WhatsApp message: {}", e.getMessage());
+        }
+    }
 
-        Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("whatsapp:" + toNumber),
-                new com.twilio.type.PhoneNumber("whatsapp:"+ twilioConfig.getWhatsappNumber()),
-                messageContent)
-                        .create();
-        logger.info("Whatsapp Message Status: {}", message.getStatus());
+    private Message sendWhatsAppMessage(String messageContent, String toNumber) {
+        return Message.creator(
+                        new com.twilio.type.PhoneNumber("whatsapp:" + toNumber),
+                        new com.twilio.type.PhoneNumber("whatsapp:" + twilioConfig.getWhatsappNumber()),
+                        messageContent)
+                .create();
     }
 }
