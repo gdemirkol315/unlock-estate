b5ff5597e8741517b47bc484c34f2e6e6f0c4a5e
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-04-21 12:04:42 +0200

    if user already exists email is not sent

--- back-end/ueparent/src/main/java/com/unlockestate/ueparent/user/service/AuthenticationService.java
+++ back-end/ueparent/src/main/java/com/unlockestate/ueparent/user/service/AuthenticationService.java
@@ -74,6 +74,10 @@ public class AuthenticationService {
         salt.setSaltString(createSalt());
         salt.setEmail(request.getEmail());
 
+        saltRepository.save(salt);
+        user = userRepository.save(user);
+        String token = jwtService.generateToken(user);
+
         if (user.getEmail().equals(initialUser)) {
             user.setPassword(passwordEncoder.encode(request.getPassword() + salt.getSaltString()));
         } else {
@@ -89,9 +93,6 @@ public class AuthenticationService {
             user.setPassword(passwordEncoder.encode(tmpPassword + salt.getSaltString()));
         }
 
-        saltRepository.save(salt);
-        user = userRepository.save(user);
-        String token = jwtService.generateToken(user);
         return new AuthenticationResponse(token);
     }
 
